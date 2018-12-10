ab9e379b2aafbbb464069f34dd52c7dd7c4d92f2
Yawar Amin <yawar.amin@gmail.com>
2018-12-09 20:12:56 -0500

    Add test for array of wrapped values
    Remove redundant test. Factor out common value.

--- src/test/scala/newtype/NewtypeSpec.scala
+++ src/test/scala/newtype/NewtypeSpec.scala
@@ -5,19 +5,24 @@ import org.scalatest.refspec.RefSpec
 class NewtypeSpec extends RefSpec {
   import NewtypeSpec._
 
-  def `preserves value` = assertResult(One)(Id.value(Id(One)))
-  def `wrapper has identical memory layout to input value` =
-    assertResult(bytes(One))(bytes(Id(One)))
-  def `wrapper is type-incompatible with input value` =
-    assertTypeError("Id(One).capitalize")
+  /* This proves that putting the wrapped values into an array doesn't
+     box them. */
+  def `array of wrapped values has identical memory layout to array of input values` =
+    assertResult(bytes(Array(One)))(bytes(Array(idOne)))
   def `errors if invariants violated` =
     assertThrows[IllegalArgumentException](Id(Zero))
+  def `wrapper has identical memory layout to input value` =
+    assertResult(bytes(One))(bytes(idOne))
+  def `wrapper is type-incompatible with input value` =
+    assertTypeError("idOne.capitalize")
 }
 
 object NewtypeSpec {
+  import scala.reflect.ClassTag
+
   val Zero = "0"
-  val One = "1"
-  val Id = Newtype[String](Zero.!=)
+  val Id: Newtype[String] = Newtype(Zero.!=)
+  type Id = Id.Type
 
   // Thanks to https://stackoverflow.com/a/39371571/20371
   def bytes(any: Any): Array[Byte] = {
@@ -30,4 +35,8 @@ object NewtypeSpec {
     objectOutputStream.close()
     byteArrayOutputStream.toByteArray
   }
+
+  implicit val classTagId: ClassTag[Id] = Id.classTag
+  val One = "1"
+  val idOne = Id(One)
 }
